0232cfda248a26e38be2b3c3679fcea507c516dd
dpkashyap3 <dpkashyap3@hotmail.com>
2020-08-21 17:47:53 -0700

    Updates

---
+++ precache-manifest.d760c6bad840e133e10c0bddc5656f8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ed29c703f39b643e4af7ffe2505cdf",
+    "url": "/starwars_withreactquery/index.html"
+  },
+  {
+    "revision": "3bbc21c542ee3978f02d",
+    "url": "/starwars_withreactquery/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f1a5ad6867ca2c49f6bb",
+    "url": "/starwars_withreactquery/static/css/main.cc2c4ca3.chunk.css"
+  },
+  {
+    "revision": "3bbc21c542ee3978f02d",
+    "url": "/starwars_withreactquery/static/js/2.02bb26bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starwars_withreactquery/static/js/2.02bb26bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a5ad6867ca2c49f6bb",
+    "url": "/starwars_withreactquery/static/js/main.ef3a0f34.chunk.js"
+  },
+  {
+    "revision": "fa059bcf8aeb4e689847",
+    "url": "/starwars_withreactquery/static/js/runtime-main.7745a13a.js"
+  }
+]);
